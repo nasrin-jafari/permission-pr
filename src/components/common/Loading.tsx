d7e6bda68f6c286bca334eae0bb01c0435241c9a
nasrin <nasrin.jafari778@gmail.com>
2024-12-27 15:23:32 +0330

    feat: add Loading components

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,7 @@
+const Loading = () => (
+  <div className="flex items-center justify-center">
+    <div className="h-12 w-12 animate-spin rounded-full border-y-2 border-solid border-blue-500 border-t-transparent shadow-md"></div>
+  </div>
+);
+
+export default Loading;
