b46df04302f0db68f57376ab9874f7e30646a8ab
nasrin <nasrin.jafari778@gmail.com>
2024-12-27 14:47:18 +0330

    feat: integrate React Query with QueryClientProvider in MainLayout

--- src/components/providers/MainLayout.tsx
+++ src/components/providers/MainLayout.tsx
@@ -1,13 +1,19 @@
+'use client';
 import { MainLayoutProps } from '@/types';
 import Navbar from '@/components/sections/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function MainLayout({ children }: MainLayoutProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="bg-dark-bg flex min-h-screen flex-col items-center justify-start pb-10">
+    <div className="flex min-h-screen flex-col items-center justify-start bg-dark-bg pb-10">
       <Navbar />
-      <div className="bg-dark-foreground flex min-h-[84vh] w-[90%] items-center justify-center rounded-lg p-6 text-white shadow-lg dark:bg-gray-800">
-        {children}
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex min-h-[84vh] w-[90%] items-center justify-center rounded-lg bg-dark-foreground p-6 text-white shadow-lg dark:bg-gray-800">
+          {children}
+        </div>
+      </QueryClientProvider>
     </div>
   );
 }
