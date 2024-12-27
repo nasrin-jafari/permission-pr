3df98047d4574adb6f24b998ea31efff8d058c34
nasrin <nasrin.jafari778@gmail.com>
2024-12-27 15:24:22 +0330

    feat: add setting page

---
+++ src/app/setting/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Page = () => <p>این صفحه در دست ساخت است . لطفا صبور باشید!</p>;
+
+export default Page;
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,13 @@ import { ReactNode } from 'react';
 export interface MainLayoutProps {
   children: ReactNode;
 }
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface FetchPostsResponse {
+  data: Post[];
+  totalCount: number;
+}
