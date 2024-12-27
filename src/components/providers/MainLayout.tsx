7544c3d37b59e2d6e6a091d66929d45cc80d88e5
nasrin <nasrin.jafari778@gmail.com>
2024-12-27 14:34:22 +0330

    fix : format prettier MainLayout.tsx

--- src/components/providers/MainLayout.tsx
+++ src/components/providers/MainLayout.tsx
@@ -3,9 +3,9 @@ import Navbar from '@/components/sections/Navbar';
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start bg-dark-bg pb-10">
+    <div className="bg-dark-bg flex min-h-screen flex-col items-center justify-start pb-10">
       <Navbar />
-      <div className="flex min-h-[84vh] w-[90%] items-center justify-center rounded-lg bg-dark-foreground p-6 text-white shadow-lg dark:bg-gray-800">
+      <div className="bg-dark-foreground flex min-h-[84vh] w-[90%] items-center justify-center rounded-lg p-6 text-white shadow-lg dark:bg-gray-800">
         {children}
       </div>
     </div>
