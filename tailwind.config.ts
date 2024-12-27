1bf869777dea178e6c84cc8120476282d4ac3276
nasrin <nasrin.jafari778@gmail.com>
2024-12-27 14:18:18 +0330

    chore: set up Tailwind CSS and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,9 +12,15 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        primary: '#516FE9',
+        'dark-bg': '#161B1F',
+        'dark-foreground': '#1E2327',
+        'dark-primary': '#bb86fc',
+        'dark-secondary': '#03dac6',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
