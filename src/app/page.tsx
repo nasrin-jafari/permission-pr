927979ea9910262c64928e728cd19d28fe4c861a
jafari_n <jafari_n@cyberno.ir>
2024-12-28 11:55:09 +0330

    pref : Extract the function call from inside JSX to outside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,12 @@ import useGreeting from '@/hooks/useGreeting';
 
 const Dashboard: React.FC = () => {
   const currentTime = useIranTime();
+  const time = currentTime.toLocaleTimeString('fa-IR');
   const greeting = useGreeting(currentTime.getHours());
 
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <h1 className="mb-4 text-6xl font-bold">
-        {currentTime.toLocaleTimeString('fa-IR')}
-      </h1>
+      <h1 className="mb-4 text-6xl font-bold">{time}</h1>
       <h2 className="text-2xl">{greeting}</h2>
     </div>
   );
